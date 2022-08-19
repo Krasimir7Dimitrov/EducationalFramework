26226a348b2e24340cd25ce4f836ef12775ff976
Pancho Sabev <psabev@parachut.com>
2022-08-19 12:10:53 +0300

    add logic to make api calls to reqres api

---
+++ app/Controllers/UsersController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controllers;
+
+use App\System\AbstractController;
+use GuzzleHttp\Client;
+
+class UsersController extends AbstractController
+{
+
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function getAllUsers()
+    {
+        $client =  new Client();
+
+        try {
+            $response = $client->request('get', 'https://reqres.in/api/users?page=1');
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+        }
+
+        $users = json_decode($response->getBody());
+
+        var_dump($response->getStatusCode());
+
+        $this->renderView('/users/list', ['users' => $users->data]);
+    }
+
+    public function getUserById()
+    {
+        $client = new Client();
+        $id = $_GET['id'];
+
+        try {
+            $response = $client->request('get', 'https://reqres.in/api/users/'. $id);
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+        }
+
+        $users = json_decode($response->getBody());
+
+        var_dump($response->getStatusCode());
+
+        $this->renderView('/users/single', ['users' => $users->data]);
+    }
+
+    public function create()
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->request('post', 'https://reqres.in/api/users', [
+                'json' => [
+                    'first_name' => $_POST['first_name'],
+                    'last_name'  => $_POST['last_name'],
+                    'email'      => $_POST['email'],
+                ]
+            ]);
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+        }
+
+        $users = json_decode($response->getBody());
+
+        var_dump($users);
+        var_dump($response->getStatusCode());
+
+        $this->renderView('/users/create', ['users' => $users->data]);
+    }
+
+    public function update()
+    {
+        $client = new Client();
+        $id = $_POST['id'];
+
+        try {
+            $response = $client->request('put', 'https://reqres.in/api/users/'. $id , [
+                'json' => [
+                    'id'         => $_POST['id'],
+                    'first_name' => $_POST['first_name'],
+                    'last_name'  => $_POST['last_name'],
+                    'email'      => $_POST['email'],
+                    'avatar'     => $_POST['avatar']
+                ]
+            ]);
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+        }
+
+        $users = json_decode($response->getBody());
+
+        var_dump($response->getStatusCode());
+
+
+        $this->renderView('/users/single', ['users' => $users]);
+    }
+
+    public function delete()
+    {
+        $client = new Client();
+        $id = $_GET['id'];
+
+        try {
+            $response = $client->request('delete', 'https://reqres.in/api/users/'. $id);
+            $usersList = $client->request('get', 'https://reqres.in/api/users?page=1');
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+        }
+
+        $users = json_decode($usersList->getBody());
+        $deleteResponse = json_decode($response->getBody());
+
+        var_dump($deleteResponse);
+        var_dump($response->getStatusCode());
+
+
+        $this->renderView('/users/list', ['users' => $users->data]);
+    }
+
+}
