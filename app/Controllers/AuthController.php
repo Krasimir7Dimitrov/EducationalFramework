954057146c6dd9cdd91effac682cff0022c7e4ae
kdimitrov <kdimitrov@parachut.com>
2022-08-21 00:51:21 +0300

    add reset password

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Model\Collections\UsersCollection;
 use App\System\AbstractController;
 use App\System\Registry;
 use App\System\Traits\Auth;
+use DateTime;
+use mysql_xdevapi\Exception;
 
 class AuthController extends AbstractController
 {
@@ -35,6 +37,11 @@ class AuthController extends AbstractController
             $username = $_POST['username'] ?? '' ;
             $password = $_POST['password'] ?? '' ;
             $row = $this->usersCollection->getUserByUsername($username);
+
+            if ($row['password'] !== sha1($password)) {
+                $this->setFlashMessage('Your password is incorrect.');
+                $this->redirect('auth', 'login');
+            }
             if ($row['is_verified'] != 1) {
                 $this->setFlashMessage('Your account is not activated. Please check your email for verification code.');
                 $this->redirect('auth', 'login');
@@ -70,19 +77,33 @@ class AuthController extends AbstractController
             elseif (strlen($regData['username']) < 4) {
                 $errors['username'] = 'Your Username Must Contain At Least 3 Characters!';
             }
+            elseif (strlen($regData['username']) > 20) {
+                $errors['username'] = 'Your Username Must Contain not more than 20 Characters!';
+            }
             elseif (!ctype_alpha($regData['username'])) {
                 $errors['username'] = 'Username must contain only letters';
             }
+            elseif (!empty($this->usersCollection->checkUsernameExist($regData['username']))) {
+                $errors['username'] = 'This username already exists';
+            }
             $data['username'] = $regData['username'];
+
             if (!filter_var($regData['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors['username'] = 'It is not valid email';
+                $errors['email'] = 'It is not valid email';
+            }
+            elseif (!empty($this->usersCollection->checkEmailExist($regData['email']))) {
+                $errors['email'] = 'This email already exists';
             }
             $data['email'] = $regData['email'];
+
             if (empty($regData['password'])) {
                 $errors['password'] = 'Password field is required';
             }
             elseif (strlen($regData['password']) < 8) {
                 $errors['password'] = 'Your Password Must Contain At Least 8 Characters!';
+            }
+            elseif (strlen($regData['password']) > 30) {
+                $errors['password'] = 'Your Password Must Contain not more than 30 Characters!';
             }
             elseif(!preg_match("#[0-9]+#",$regData['password'])) {
                 $errors['password'] = "Your Password Must Contain At Least 1 Number!";
@@ -142,6 +163,100 @@ class AuthController extends AbstractController
         }
     }
 
+    public function reset()
+    {
+        $regData = $_POST;
+        $errors = [];
+        $data = [];
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == 'POST') {
+            if (!filter_var($regData['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'It is not valid email';
+            }
+            $data['email'] = $regData['email'];
+            if (!$this->usersCollection->checkEmailExist($regData['email'])) {
+                $errors['email'] = 'We can not find user with that email address.';
+            }
+            if (empty($errors)) {
+                $str = $this->generateRandomString(30);
+                $this->usersCollection->deleteIfEmailExist($regData['email']);
+                $data['email'] = $regData['email'];
+                $data['token'] = $str;
+                $this->usersCollection->createToken($data);
+
+                $link = "<div><a href='{$this->url}auth/change?email={$regData['email']}&token={$str}'>Click</a></div>";
+                $mail = new \App\System\Email\Email();
+                $mail->to = $regData['email'];
+                $mail->subject = 'This is your verification code';
+                $mail->body = $link;
+                $send = new \App\System\Email\NotificationEmail($mail);
+                $send->send();
+
+                $this->setFlashMessage('Please check email for reset password link.');
+                $this->redirect('auth', 'login');
+            }
+        }
+
+        $this->renderView('auth/reset', ['url' => $this->url, 'errors' => $errors, 'data' => $data]);
+    }
+
+    public function change()
+    {
+        $query = $_GET;
+        $token = $this->usersCollection->getDataFromPassReset($query);
+        if (empty($token)) {
+            $this->redirect('auth', 'login');
+        }
+
+        $datetime = new DateTime();
+        $now = $datetime->format('Y-m-d H:i:s');
+        $time = strtotime($now) - strtotime($token['created_at']);
+        if (($time / 3600) > 1) {
+            $this->setFlashMessage('Your reset link is expired.');
+            $this->redirect('auth', 'login');
+        }
+        $errors = [];
+        $regData = $_POST;
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == 'POST') {
+            if (empty($regData['password'])) {
+                $errors['password'] = 'Password field is required';
+            }
+            elseif (strlen($regData['password']) < 8) {
+                $errors['password'] = 'Your Password Must Contain At Least 8 Characters!';
+            }
+            elseif (strlen($regData['password']) > 30) {
+                $errors['password'] = 'Your Password Must Contain not more than 30 Characters!';
+            }
+            elseif(!preg_match("#[0-9]+#",$regData['password'])) {
+                $errors['password'] = "Your Password Must Contain At Least 1 Number!";
+            }
+            elseif(!preg_match("#[A-Z]+#",$regData['password'])) {
+                $errors['password'] = "Your Password Must Contain At Least 1 Capital Letter!";
+            }
+            elseif(!preg_match("#[a-z]+#",$regData['password'])) {
+                $errors['password'] = "Your Password Must Contain At Least 1 Lowercase Letter!";
+            }
+
+            if ($regData['password'] !== $regData['repeat_password']) {
+                $errors['repeatPass'] = "Password and Confirm Password must be same!";
+            }
+
+            if (empty($errors)) {
+                try {
+                    $this->usersCollection->update(['password' => sha1($regData['password'])], ['email' => $query['email']]);
+                } catch (\Throwable $e) {
+                    $e->getMessage();
+                    $this->setFlashMessage('Something went wrong.');
+                    $this->redirect('auth', 'login');
+                }
+                $this->setFlashMessage('Your password was change successfully.');
+                $this->redirect('auth', 'login');
+            }
+        }
+        $this->renderView('auth/change', ['url' => $this->url, 'errors' => $errors]);
+    }
+
     public function logout()
     {
         $method = $_SERVER['REQUEST_METHOD'];
@@ -154,7 +269,7 @@ class AuthController extends AbstractController
         header("Location: {$redirectUrl}"); die();
     }
 
-    function generateRandomString($length = 20) {
+    private function generateRandomString($length = 20) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
         $randomString = '';
@@ -163,5 +278,4 @@ class AuthController extends AbstractController
         }
         return $randomString;
     }
-
 }
--- app/Model/Collections/UsersCollection.php
+++ app/Model/Collections/UsersCollection.php
@@ -15,12 +15,17 @@ class UsersCollection extends \App\System\BaseCollection
 
     public function create($data)
     {
-        return $this->db->insert($this->table, $data);
+        return $this->db->insert($this->table, [$data]);
+    }
+
+    public function update($data, $where)
+    {
+        return $this->db->update($this->table, $data, $where);
     }
 
     public function getVerificationCode($id)
     {
-        $sql = "SELECT u.is_verified, u.verification_code FROM $this->table AS u WHERE u.id = $id ";
+        $sql = "SELECT u.is_verified, u.verification_code FROM $this->table AS u WHERE u.id = :id ";
 
         return $this->db->fetchOne($sql, ['id' => $id]);
     }
@@ -28,5 +33,42 @@ class UsersCollection extends \App\System\BaseCollection
     public function updateIsVerified($id)
     {
         return $this->db->update($this->table, ['is_verified' => 1], ['id' => $id]);
+    }
+
+    public function checkUsernameExist($username)
+    {
+        $sql = "SELECT u.id, u.username FROM $this->table AS u WHERE u.username = :username";
+
+        return $this->db->fetchOne($sql, ['username' => $username]);
+    }
+
+    public function checkEmailExist($email)
+    {
+        $sql = "SELECT u.id, u.email FROM $this->table AS u WHERE u.email = :email";
+
+        return $this->db->fetchOne($sql, ['email' => $email]);
+    }
+
+    public function deleteIfEmailExist($email)
+    {
+        return $this->db->delete('password_resets', ['email' => $email]);
+    }
+
+    public function createToken($data)
+    {
+        return $this->db->insert('password_resets', $data);
+    }
+
+    public function getDataFromPassReset($where)
+    {
+        $sql = "SELECT * FROM password_resets AS pass WHERE 1";
+        $data = [];
+        foreach ($where as $key => $value) {
+            if (!empty($value)) {
+                $data[$key] = $value;
+                $sql .= " AND pass.{$key} = :{$key}";
+            }
+        }
+        return $this->db->fetchOne($sql, $data);
     }
 }
